14f8162ebdec1f14855b0b64c369f87dbd65672c
losspamerianos <sven-bredow@gmx.de>
2025-01-07 18:37:19 +0100

    -

--- app/main.py
+++ app/main.py
@@ -17,13 +17,13 @@ app = FastAPI(
 # ---------------------------------------------------
 # CORS Middleware
 # ---------------------------------------------------
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=settings.cors_origins_list,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
 # ---------------------------------------------------
 # Auth / Secret Middleware
